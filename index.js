5ac26bfbd34919a31fad7cf8e800bfeb44d96dd4
MullahStanley <mullah.kimani@student.moringaschool.com>
2023-03-15 21:30:18 +0300

    Assignment complete

---
+++ index.js
@@ -0,0 +1,18 @@
+//function called divide that divides 2000 by 100
+function divide(num1,num2){
+  return 2000/100
+}
+divide();
+//arrow function called square
+let num=2;
+const square=(num)=>{
+    return (num*num)
+}
+square();
+//arrow function called add
+let a=3;
+let b=4;
+const add=(a,b)=>{
+    return a+b;
+}
+add();
